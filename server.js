ec3f6fe8178bc7216622ddb5b64dba878fb10722
Tevin Gampalage <tevingampalage29@gmail.com>
2026-09-23 13:06:42 +0530

    love

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Error loading file');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
